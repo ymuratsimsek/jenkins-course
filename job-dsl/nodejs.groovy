4389f772c83da8074fd9a5e0e4c0e56010a4f374
Murat <15009851+ymuratsimsek@users.noreply.github.com>
2023-08-31 13:28:08 -0400

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example') {
+job('NodeJS (build local with seed project') {
     scm {
         git('https://github.com/ymuratsimsek/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
